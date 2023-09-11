fa929479b054fbcf8fb180c7b373cb5be4809e7b
MacroModel <liyn_xlfd@163.com>
2023-09-11 20:25:00 +0800

    update parameter

--- src/phy_engine/console/command_line.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-namespace phy_engine::console {
-
-constexpr int command_line(int, char**) noexcept {
-	return 0;
-}
-}
--- src/phy_engine/main.cpp
+++ src/phy_engine/main.cpp
@@ -4,8 +4,8 @@
 
 #include "../phy_engine_utils/fast_io/fast_io.h" // fast_io
 #include "devices/native_io.h" // native_io
-#include "console/impl.h" // set_console_cp_to_utf8
+
 
 int main(int argc, char** argv) noexcept {
-	return ::phy_engine::console::command_line(argc, argv);
+	return 0;
 }
