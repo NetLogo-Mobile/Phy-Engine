2b8d4fbff251aeb537b07613f0f89c7a76fe6997
MacroModel <liyn_xlfd@163.com>
2023-10-06 22:42:36 +0800

    add convergence

--- src/phy_engine_model/model_refs/base.h
+++ src/phy_engine_model/model_refs/base.h
@@ -41,23 +41,57 @@ struct model_derv_impl : module_base_impl {
 
 	virtual constexpr module_base_impl *clone() const noexcept override {};
 
-	virtual constexpr bool init_model() noexcept override {}
-	virtual constexpr bool prepare_ac() noexcept override {}
-	virtual constexpr bool prepare_dc() noexcept override {}
-	virtual constexpr bool prepare_tr() noexcept override {}
-	virtual constexpr bool prepare_op() noexcept override {}
-	virtual constexpr bool prepare_trop() noexcept override {}
-	virtual constexpr bool iterate_ac(double omega) noexcept override {}
-	virtual constexpr bool iterate_dc() noexcept override {}
-	virtual constexpr bool iterate_tr(double tTime) noexcept override {}
-	virtual constexpr bool iterate_op() noexcept override {}
-	virtual constexpr bool iterate_trop() noexcept override {}
-	virtual constexpr bool save_op() noexcept override {}
-	virtual constexpr bool load_temperature(double temp) noexcept override {}
-	virtual constexpr bool step_changed_tr(double tTemp, double nstep) noexcept override {}
-	virtual constexpr bool adapt_step(double &step) noexcept override {}
-	virtual constexpr bool check_convergence() noexcept override {}
-	virtual constexpr ::phy_engine::model::pin_view get_pins() noexcept override {}
+	virtual constexpr bool init_model() noexcept override {
+		return ::phy_engine::model::init_model<mod>(m);
+	}
+	virtual constexpr bool prepare_ac() noexcept override { 
+		return ::phy_engine::model::prepare_ac<mod>(m); 
+	}
+	virtual constexpr bool prepare_dc() noexcept override {
+		return ::phy_engine::model::prepare_dc<mod>(m);
+	}
+	virtual constexpr bool prepare_tr() noexcept override {
+		return ::phy_engine::model::prepare_tr<mod>(m);
+	}
+	virtual constexpr bool prepare_op() noexcept override {
+		return ::phy_engine::model::prepare_op<mod>(m);
+	}
+	virtual constexpr bool prepare_trop() noexcept override {
+		return ::phy_engine::model::prepare_trop<mod>(m);
+	}
+	virtual constexpr bool iterate_ac(double omega) noexcept override {
+		return ::phy_engine::model::iterate_ac<mod>(m, omega);
+	}
+	virtual constexpr bool iterate_dc() noexcept override {
+		return ::phy_engine::model::iterate_dc<mod>(m);
+	}
+	virtual constexpr bool iterate_tr(double tTime) noexcept override {
+		return ::phy_engine::model::iterate_tr<mod>(m, tTime);
+	}
+	virtual constexpr bool iterate_op() noexcept override {
+		return ::phy_engine::model::iterate_op<mod>(m);
+	}
+	virtual constexpr bool iterate_trop() noexcept override {
+		return ::phy_engine::model::iterate_trop<mod>(m);
+	}
+	virtual constexpr bool save_op() noexcept override {
+		return ::phy_engine::model::save_op<mod>(m);
+	}
+	virtual constexpr bool load_temperature(double temp) noexcept override {
+		return ::phy_engine::model::load_temperature<mod>(m, temp);
+	}
+	virtual constexpr bool step_changed_tr(double tTemp, double nstep) noexcept override {
+		return ::phy_engine::model::step_changed_tr<mod>(m, tTemp, nstep);
+	}
+	virtual constexpr bool adapt_step(double &step) noexcept override {
+		return ::phy_engine::model::adapt_step<mod>(m, step);
+	}
+	virtual constexpr bool check_convergence() noexcept override {
+		return ::phy_engine::model::check_convergence<mod>(m);
+	}
+	virtual constexpr ::phy_engine::model::pin_view get_pins() noexcept override {
+		return ::phy_engine::model::get_pins<mod>(m);
+	}
 };
 }
 
